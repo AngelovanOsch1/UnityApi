450842b2a7f90e7021d6bbde1aee6a35e5839aea
AngelovanOsch1 <99641678+AngelovanOsch1@users.noreply.github.com>
2023-01-23 23:19:02 +0100

    1/23

--- config/dev.js
+++ config/dev.js
@@ -1,5 +1,5 @@
 module.exports = {
-    port: 3000,
+    port: 13756,
     mongoURI: 'mongodb+srv://AngelovanOsch:9zlduWQyU9QxB15v@flappygame.pp4xasn.mongodb.net/test?retryWrites=true&w=majority',
 };
 
